961b7fcf4778c9e2e9dbd910ff49c6b52cec5508
Valdécio <valdecio@atlantasistemas.com.br>
2022-05-29 16:49:27 -0300

    Criando a classe posição

--- xadres-console/Program.cs
+++ xadres-console/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-
+using Tabuleiro;
 namespace xadres_console // Note: actual namespace depends on the project name.
 {
     internal class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        static void Main(string[] args)
+        {
+             Posicao P;
+
+             P = new Posicao(3, 4);
+
+             Console.WriteLine("Posição: " + P);
+            
+        }
     }
-}
 }
